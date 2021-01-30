a2251c15c3839c1f8d81687ed38092cc45a2a46d
tattoo197@outlook.com <papon@wecomics.in.th>
2021-01-30 16:28:50 +0700

    create DB if call openDatabase

--- lib/local_database/transaction_db.dart
+++ lib/local_database/transaction_db.dart
@@ -1,5 +1,7 @@
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
 
 class TransactionInFoDB {
   //บริการเกี่ยวกับฐานข้อมูล
@@ -11,11 +13,15 @@ class TransactionInFoDB {
   TransactionInFoDB(this.dbName);
 
 //dbName = transanction.db
-  Future<String> openDatabase() async {
+  Future<Database> openDatabase() async {
     //หาตำแหน่งที่จะเก็บข้อมูล
     var appDirectory = await getApplicationDocumentsDirectory();
     String dbLocation =
         join(appDirectory.path, dbName); //(hostpath,transanction.db)
-    return dbLocation;
+
+    //สร้าง DB
+    DatabaseFactory dbFactory = await databaseFactoryIo;
+    Database db = await dbFactory.openDatabase(dbLocation);
+    return db;
   }
 }
