7f5d746fd8cbee49b34137921a2f1286bdbfab66
tattoo197@outlook.com <papon@wecomics.in.th>
2021-02-01 21:13:28 +0700

    add home_screen widget for customize view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:basic_command_app/models/TransactionInfos.dart';
 import 'package:basic_command_app/provider/transaction_provider.dart';
 import 'package:basic_command_app/screen/form_screen.dart';
+import 'package:basic_command_app/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
@@ -47,74 +48,8 @@ class HomeBodyContainer extends StatefulWidget {
 
 class _HomeBodyContainerState extends State<HomeBodyContainer> {
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    Provider.of<TransactionMemberProvider>(context, listen: false).initData();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text(
-          "รายชื่อสมาชิก",
-          style: TextStyle(color: Colors.black),
-        ),
-        backgroundColor: Colors.white,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            color: Colors.black,
-            onPressed: () {
-              print("Show Add");
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return FormScreen();
-              }));
-            },
-          )
-        ],
-      ),
-      body: Consumer(
-        builder: (context, TransactionMemberProvider provider, Widget child) {
-          var count = provider.memberTransaction.length;
-          if (count > 0) {
-            return ListView.builder(
-              itemCount: count,
-              itemBuilder: (context, int index) {
-                TransactionInFos memberInfo = provider.memberTransaction[index];
-
-                return Card(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                  elevation: 10,
-                  child: ListTile(
-                    trailing: Text(memberInfo.phone.toString()),
-                    leading: CircleAvatar(
-                      radius: 22,
-                      child: FittedBox(
-                        child: Text(memberInfo.score.toString()),
-                      ),
-                    ),
-                    title: Text(memberInfo.name + " " + memberInfo.surname),
-                    subtitle:
-                        Text(DateFormat("dd-mm-yyyy").format(memberInfo.date)),
-                  ),
-                );
-              },
-            );
-          } else {
-            return Center(
-              child: Text(
-                "ไม่พบข้อมูล",
-                style: TextStyle(fontSize: 35),
-              ),
-            );
-          }
-        },
-      ),
-    );
+    return HomeScreen();
   }
 }
